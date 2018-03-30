32ce9e1b63a3f729d52522c87c9612a228ac7678
Lucasrz <zalaf.lucas@gmail.com>
2018-03-29 22:02:01 -0300

    feat(cash-machine): Add view to the cash machine
    In this commit we add an interface at '/' where the user can withdraw any amount he wants, as log as it is a valid number and the machine has the correct bills.

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,11 @@ var cashMachineController = require('./cash-machine')
 
 router.post('/api/cash-machine', cashMachineController.post)
 
+router.post('/', cashMachineController.viewPost)
+
 router.get('/', function (req, res) {
   res.render('index')
 })
+
 
 module.exports = router
